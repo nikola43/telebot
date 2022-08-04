6122b83ae8ce5f0e08e891424bd1c54d137f407a
paulo <pauloxti@gmail.com>
2022-08-04 21:52:21 +0200

    bot

--- src/main.rs
+++ src/main.rs
@@ -17,28 +17,31 @@ async fn main() -> web3::Result<()> {
     let mut initial_balance: U256 = U256::from(0);
     let seconds = time::Duration::from_secs(1);
 
-    let mut bags: Vec<String> = vec!["".into()];
-
     while true {
         thread::sleep(seconds);
         let balance = web3s.eth().balance(accounts[0], None).await?;
         println!("Balance of {:?}: {}", accounts[0], balance);
-
-        for i in 0..50 {
+        let mut bags: Vec<String> = vec!["".into()];
+        for _ in 0..50 {
             let borrowed_string = "💰";
             bags.push(borrowed_string.to_string())
         }
 
-        let mut temp_text: Vec<String> = vec![
+        let mut invested: String = "    👉 Invested: ".into();
+        let amount = round(wei_to_eth(balance)).to_string();
+        invested.push_str(amount.as_str());
+        invested.push_str("BNB of 50 BNB\n\n");
+
+        let temp_text: Vec<String> = vec![
             bags.join(""),
+            " \n".into(),
+            " \n".into(),
+            "      🔥🔥🔥New investment🚀🚀🚀\n\n".into(),
+            invested,
+            "       Fill this form after investment\n https://forms.gle/tj8Hd6TpGa3R8Bf59\n\n".into(),
+            "Send your investment to this wallet:\n
+            0x9aA7CE0aAb87C1Ef52E0e081CF9A51aad93d3bF0\n\n
             "
-        🔥🔥🔥¡¡¡¡¡New investment in Metastock!!!!!🚀🚀🚀"
-                .into(),
-            "👉 Invested: 0.5 BNB of 50 BNB
-        Fill this form: https://forms.gle/tj8Hd6TpGa3R8Bf59
-        Send your investment to this wallet:
-        0x9aA7CE0aAb87C1Ef52E0e081CF9A51aad93d3bF0
-        "
             .into(),
         ];
 
@@ -48,4 +51,20 @@ async fn main() -> web3::Result<()> {
         }
     }
     Ok(())
+}
+
+pub fn wei_to_eth(wei_val: U256) -> f64 {
+    let res = wei_val.as_u128() as f64;
+    res / 1_000_000_000_000_000_000.0
+}
+
+pub fn eth_to_wei(eth_val: f64) -> U256 {
+    let result = eth_val * 1_000_000_000_000_000_000.0;
+    let result = result as u128;
+    U256::from(result)
+}
+
+fn round(x: f64) -> f64 {
+    let r = (x * 100.0).round() / 100.0;
+    return r;
 }
